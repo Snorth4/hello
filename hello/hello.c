b9f4615595b3034a05108fdf4782fc510f3089e8
Stephen North <snorth4@huskers.unl.edu>
2021-08-27 19:07:21 +0000

    initial commit

--- hello/hello.c
+++ hello/hello.c
@@ -1,2 +1,18 @@
-printf("Stephen North!")
-printf("Computer Science")
+
+ /**
+  * Author: Stephen North
+  * Date: 2021/08/24
+  *
+  * A simple hello world program in C
+  *
+  */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+    printf ("Stephen North!\n");
+    printf ("COMPUTER SCIENCE!\n");
+
+    return 0;
+}
